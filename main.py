1977ab42eef5b75283716aeda197211ce5035cb6
Jagoda Jeczmien-Lazur <jagoda.jeczmienlazur@gmail.com>
2022-08-18 21:10:12 +0200

    Zadanie 1

--- main.py
+++ main.py
@@ -2,9 +2,14 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# @app.route('/')
+# def homepage():
+#     return render_template("index.html")
+
 @app.route('/')
 def homepage():
-    return render_template("index.html")
+    movies = ["Mroczny zakatek", "Amelia", "Gwiezdny pyl"]
+    return render_template("homepage.html", movies=movies)
 
 if __name__ == '__main__':
     app.run(debug=True)

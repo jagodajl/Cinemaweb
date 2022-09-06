4cdb601843218df37ce1d796437cdaa39c3973d7
Jagoda Jeczmien-Lazur <jagoda.jeczmienlazur@gmail.com>
2022-09-06 09:49:54 +0200

    Parametryzacja test_homepage z list_type oraz wbudowanym we Flask klientem.

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,12 +1,15 @@
+import pytest
+
 from main import app
 from unittest.mock import Mock
 
 
-def test_homepage(monkeypatch):
+@pytest.mark.parametrize("list_type", ("top_rated", "upcoming", "popular", "now_playing"))
+def test_homepage(monkeypatch, list_type):
     api_mock = Mock(return_value={"results": []})
     monkeypatch.setattr("tmdb_client.get_list_type_movies", api_mock)
 
     with app.test_client() as client:
-        response = client.get("/")
+        response = client.get(f"/?list_type={list_type}")
         assert response.status_code == 200
-        api_mock.assert_called_once_with("popular")
+        api_mock.assert_called_once_with(list_type)

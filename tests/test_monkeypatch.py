8dba0be56de86cb113ec9cba6011903317973206
Jagoda Jeczmien-Lazur <jagoda.jeczmienlazur@gmail.com>
2023-01-22 12:31:30 +0100

    Poprawa testu monkeypaych

--- tests/test_monkeypatch.py
+++ tests/test_monkeypatch.py
@@ -3,12 +3,19 @@ from unittest.mock import Mock
 import tmdb_client
 
 def test_fetch_movie_list(monkeypatch):
-    mock_movies_list = ['Movie 1', 'Movie 2']
+    # mock_movies_list = ['Movie 1', 'Movie 2']
+
+    # requests_mock = Mock()
+    # response = requests_mock.return_value
+    # response.json.return_value = mock_movies_list
+    # monkeypatch.setattr("tmdb_client.requests.get", requests_mock)
+    #
+    # movies_list = tmdb_client.get_list_type_movies(list_type="popular")
+    # assert movies_list == mock_movies_list
 
     requests_mock = Mock()
-    response = requests_mock.return_value
-    response.json.return_value = mock_movies_list
+    requests_mock.return_value.json.return_value = ['Movie 1', 'Movie 2']
     monkeypatch.setattr("tmdb_client.requests.get", requests_mock)
 
     movies_list = tmdb_client.get_list_type_movies(list_type="popular")
-    assert movies_list == mock_movies_list
+    assert movies_list == ['Movie 1', 'Movie 2']
